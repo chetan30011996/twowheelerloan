cf8eb1755e249bec74abb343925b59f425b32ac7
chetan30011996 <88084276+chetan30011996@users.noreply.github.com>
2021-08-27 20:42:33 +0530

    Add files via upload

---
+++ fileInitiate/src/main/java/com/cjc/app/tl/fileInitiate/main/FileInitiateApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.app.tl.fileInitiate.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileInitiateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileInitiateApplication.class, args);
+	}
+
+}
